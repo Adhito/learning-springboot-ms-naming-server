6cf4c3111573d4e35b61630d8114beb584a5894d
adhito909 <adhito909@gmail.com>
2023-07-03 22:10:32 +0700

    Create initial base ms-naming-server service source code

---
+++ src/test/java/com/learningspringbootms/msnamingserver/MsNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningspringbootms.msnamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
